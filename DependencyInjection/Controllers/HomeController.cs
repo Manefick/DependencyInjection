705505382c489cd1817fdc78ba73f6885034b42d
Alex Grinchuck <alex.grinchuck@gmail.com>
2020-09-01 21:58:08 +0300

    Add model, interfase repository, add memory repository , add view, using dependensis injection

---
+++ DependencyInjection/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DependencyInjection.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DependencyInjection.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository;//где именно при вызове интерфейса создаеться екземпляр класса реализуещего интерфейс?
+        public HomeController(IRepository repo) => repository = repo;//зачем и как оно работает
+        public ViewResult Index() => View(repository.Products);//откуда беруться даніе из интерфейса ??
+    }
+}
